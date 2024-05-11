41bac8615adfda59d6da7919829a82fde32bc9d8
Alejandro <alejanfh@gmail.com>
2024-05-11 19:37:08 +0200

    First commit, starting the project

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Petsoft - Pet daycare software",
+  description: "Take care of people's pets responsibly with Petsoft",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} text-sm text-zinc-900 bg-[#E5E8EC] min-h-screen`}
+      >
+        Header
+        {children}
+      </body>
+    </html>
+  );
+}
