1de5956ada968aaa4224c9e35310da2d019e8f4b
Alejandro Fortes Hidalgo <alejandro.fortes@mecalux.com>
2024-05-14 13:04:22 +0200

    Add dialog component in pet button

--- src/components/pet-button.tsx
+++ src/components/pet-button.tsx
@@ -1,36 +1,61 @@
-import { ReactNode } from "react";
-import { Button } from "./ui/button";
+"use client";
+
 import { PlusIcon } from "@radix-ui/react-icons";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import PetForm from "./pet-form";
+import { useState } from "react";
 
 type PetButtonProps = {
   actionType: "add" | "edit" | "checkout";
-  children?: ReactNode;
+  disabled?: boolean;
   onClick?: () => void;
+  children?: React.ReactNode;
 };
 
 export default function PetButton({
-  children,
   actionType,
+  disabled,
   onClick,
+  children,
 }: PetButtonProps) {
-  if (actionType === "add") {
-    return (
-      <Button size="icon">
-        <PlusIcon className="h-6 w-6" />
-      </Button>
-    );
-  }
-
-  if (actionType === "edit") {
-    return <Button variant="secondary">{children}</Button>;
-  }
+  const [isFormOpen, setIsFormOpen] = useState(false);
 
   if (actionType === "checkout") {
     return (
-      <Button variant="secondary" onClick={onClick}>
-        {" "}
+      <Button variant="secondary" disabled={disabled} onClick={onClick}>
         {children}
       </Button>
     );
   }
+
+  return (
+    <Dialog open={isFormOpen} onOpenChange={setIsFormOpen}>
+      <DialogTrigger asChild>
+        {actionType === "add" ? (
+          <Button size="icon">
+            <PlusIcon className="h-6 w-6" />
+          </Button>
+        ) : (
+          <Button variant="secondary">{children}</Button>
+        )}
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            {actionType === "add" ? "Add a new pet" : "Edit pet"}
+          </DialogTitle>
+        </DialogHeader>
+
+        <PetForm />
+      </DialogContent>
+    </Dialog>
+  );
 }
---
+++ src/components/pet-form.tsx
@@ -0,0 +1,3 @@
+export default function PetForm() {
+  return <form>Form here...</form>;
+}
