7eeb6d50fca00ee061e08944e90bfdf1fb831c68
Alejandro Fortes Hidalgo <alejandro.fortes@mecalux.com>
2024-05-15 09:34:09 +0200

    Add prisma database

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -3,23 +3,15 @@ import AppHeader from "@/components/app-header";
 import BackgroundPattern from "@/components/background-pattern";
 import PetContextProvider from "@/contexts/pet-context-provider";
 import SearchContextProvider from "@/contexts/search-context-provider";
-import { Pet } from "@/lib/types";
 import { ReactNode } from "react";
+import prisma from "@/lib/db";
 
 type AppLayout = {
   children: ReactNode;
 };
 
 export default async function AppLayout({ children }: AppLayout) {
-  const response = await fetch(
-    "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
-  );
-
-  if (!response.ok) {
-    throw new Error("Could not fetch pets");
-  }
-
-  const data: Pet[] = await response.json();
+  const pets = await prisma.pet.findMany({});
 
   return (
     <>
@@ -27,7 +19,7 @@ export default async function AppLayout({ children }: AppLayout) {
 
       <div className="flex flex-col max-w-[1050px] mx-auto px-4 min-h-screen">
         <AppHeader />
-        <PetContextProvider data={data}>
+        <PetContextProvider data={pets}>
           <SearchContextProvider>{children}</SearchContextProvider>
         </PetContextProvider>
 
---
+++ src/lib/db.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+// Esto es para poder instanciar prisma en cualquier parte y poder crear, eliminar,...
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
