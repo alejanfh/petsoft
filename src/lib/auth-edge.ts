a2106008358594ad2a79ba8a7b8403260cc35a36
Alejandro Fortes Hidalgo <alejandro.fortes@mecalux.com>
2024-05-24 10:45:18 +0200

    Comment payment

--- src/lib/auth-edge.ts
+++ src/lib/auth-edge.ts
@@ -21,9 +21,9 @@ export const nextAuthEdgeConfig = {
         return true;
       }
 
-      if (isLoggedIn && isTryingToAccessApp && !auth?.user.hasAccess) {
-        return Response.redirect(new URL("/payment", request.nextUrl));
-      }
+      // if (isLoggedIn && isTryingToAccessApp && !auth?.user.hasAccess) {
+      //   return Response.redirect(new URL("/payment", request.nextUrl));
+      // }
 
       if (
         isLoggedIn &&
@@ -34,16 +34,16 @@ export const nextAuthEdgeConfig = {
         return Response.redirect(new URL("/app/dashboard", request.nextUrl));
       }
 
-      if (isLoggedIn && !isTryingToAccessApp && !auth?.user.hasAccess) {
-        if (
-          request.nextUrl.pathname.includes("/login") ||
-          request.nextUrl.pathname.includes("/signup")
-        ) {
-          return Response.redirect(new URL("/payment", request.nextUrl));
-        }
+      // if (isLoggedIn && !isTryingToAccessApp && !auth?.user.hasAccess) {
+      //   if (
+      //     request.nextUrl.pathname.includes("/login") ||
+      //     request.nextUrl.pathname.includes("/signup")
+      //   ) {
+      //     return Response.redirect(new URL("/payment", request.nextUrl));
+      //   }
 
-        return true;
-      }
+      //   return true;
+      // }
 
       if (!isLoggedIn && !isTryingToAccessApp) {
         return true;
